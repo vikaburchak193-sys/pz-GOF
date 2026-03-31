5f63623b006b2f50a72d22d62da40c5362951e04
vikaburchak193-sys <vika.burchak@viti.edu.ua>
2026-03-31 13:35:10 +0300

    додала підписника

--- Burshak_GOF/behavioral/observer.ts
+++ Burshak_GOF/behavioral/observer.ts
@@ -22,9 +22,11 @@ class NewsReader implements Subscriber {
 const subject = new NewsAgency();
 const obs1 = new NewsReader("Підписник 1");
 const obs2 = new NewsReader("Підписник 2");
+const obs3 = new NewsReader("Підписник 3");
 
 subject.subscribe(obs1);
 subject.subscribe(obs2);
+subject.subscribe(obs3);
 
 console.log("--- Тест Observer ---");
 subject.notify("Нове повідомлення!");
